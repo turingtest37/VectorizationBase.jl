5ddf287e6f48eabe2f11c6ed1d9725e46516896f
a5vzener <c.difficile@gmail.com>
2020-12-02 21:52:44 -0500

    Supports absence of L3 cache.

--- src/topology.jl
+++ src/topology.jl
@@ -3,28 +3,39 @@
 const TOPOLOGY = Hwloc.topology_load();
 const CACHE = TOPOLOGY.children[1].children[1];
 const COUNTS = Hwloc.histmap(TOPOLOGY);
-# TODO: Makes topological assumptions that aren't right for
-# multiple nodes or with >3 or <3 levels of Cache.
+const CACHE_COUNT = (
+    COUNTS[:L1Cache],
+    COUNTS[:L2Cache],
+    COUNTS[:L3Cache]
+)
+const NUM_CORES = COUNTS[:Core]
+
+L1,L2,L3 = CACHE_COUNT[1:3] 
+
+# The following is amenable to simplification by metaprogramming
+attr = CACHE.children[1].children[1].attr
 const L₁CACHE = (
-    size = CACHE.children[1].children[1].attr.size,
-    depth = CACHE.children[1].children[1].attr.depth,
-    linesize = CACHE.children[1].children[1].attr.linesize,
-    associativity = CACHE.children[1].children[1].attr.associativity,
-    type = CACHE.children[1].children[1].attr.type_
+    size = L1 > 0 ? attr.size : nothing,
+    depth = L1 > 0 ? attr.depth : nothing,
+    linesize = L1 > 0 ? attr.linesize : nothing,
+    associativity = L1 > 0 ? attr.associativity : nothing,
+    type = L1 > 0 ? attr.type_ : nothing
 )
+attr = CACHE.children[1].attr
 const L₂CACHE = (
-    size = CACHE.children[1].attr.size,
-    depth = CACHE.children[1].attr.depth,
-    linesize = CACHE.children[1].attr.linesize,
-    associativity = CACHE.children[1].attr.associativity,
-    type = CACHE.children[1].attr.type_
+    size = L2 > 0 ? attr.size : nothing,
+    depth = L2 > 0 ? attr.depth : nothing,
+    linesize = L2 > 0 ? attr.linesize : nothing,
+    associativity = L2 > 0 ? attr.associativity : nothing,
+    type = L2 > 0 ? attr.type_ : nothing
 )
+attr = CACHE.attr
 const L₃CACHE = (
-    size = CACHE.attr.size,
-    depth = CACHE.attr.depth,
-    linesize = CACHE.attr.linesize,
-    associativity = CACHE.attr.associativity,
-    type = CACHE.attr.type_
+    size = L3 > 0 ? attr.size : nothing,
+    depth = L3 > 0 ? attr.depth : nothing,
+    linesize = L3 > 0 ? attr.linesize : nothing,
+    associativity = L3 > 0 ? attr.associativity : nothing,
+    type = L3 > 0 ? attr.type_ : nothing
 )
 """
 L₁, L₂, L₃ cache size
@@ -39,11 +50,3 @@ const CACHE_SIZE = (
 #     length(CACHE.children),
 #     length(TOPOLOGY.children[1].children)
 # )
-const CACHE_COUNT = (
-    COUNTS[:L1Cache],
-    COUNTS[:L2Cache],
-    COUNTS[:L3Cache]
-)
-const NUM_CORES = COUNTS[:Core]
-
-
