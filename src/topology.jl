c8d6e8eb3fa652978c56cfbbf69bfa6183e573c1
a5vzener <c.difficile@gmail.com>
2020-12-02 22:35:56 -0500

    Little niceties.

--- src/topology.jl
+++ src/topology.jl
@@ -10,32 +10,32 @@ const CACHE_COUNT = (
 )
 const NUM_CORES = COUNTS[:Core]
 
-L1,L2,L3 = CACHE_COUNT[1:3] 
+L1,L2,L3 = CACHE_COUNT[1:3] .> 0
 
 # The following is amenable to simplification by metaprogramming
-attr = L3 > 0 ? CACHE.children[1].children[1].attr : CACHE.children[1].attr
+attr = L3 ? CACHE.children[1].children[1].attr : CACHE.children[1].attr
 const L₁CACHE = (
-    size = L1 > 0 ? attr.size : nothing,
-    depth = L1 > 0 ? attr.depth : nothing,
-    linesize = L1 > 0 ? attr.linesize : nothing,
-    associativity = L1 > 0 ? attr.associativity : nothing,
-    type = L1 > 0 ? attr.type_ : nothing
+    size = L1 ? attr.size : nothing,
+    depth = L1 ? attr.depth : nothing,
+    linesize = L1 ? attr.linesize : nothing,
+    associativity = L1 ? attr.associativity : nothing,
+    type = L1 ? attr.type_ : nothing
 )
-attr = L3 > 0 ? CACHE.children[1].attr : CACHE.attr
+attr = L3 ? CACHE.children[1].attr : CACHE.attr
 const L₂CACHE = (
-    size = L2 > 0 ? attr.size : nothing,
-    depth = L2 > 0 ? attr.depth : nothing,
-    linesize = L2 > 0 ? attr.linesize : nothing,
-    associativity = L2 > 0 ? attr.associativity : nothing,
-    type = L2 > 0 ? attr.type_ : nothing
+    size = L2 ? attr.size : nothing,
+    depth = L2 ? attr.depth : nothing,
+    linesize = L2 ? attr.linesize : nothing,
+    associativity = L2 ? attr.associativity : nothing,
+    type = L2 ? attr.type_ : nothing
 )
-attr = L3 > 0 ? CACHE.attr : nothing
+attr = L3 ? CACHE.attr : nothing
 const L₃CACHE = (
-    size = L3 > 0 ? attr.size : nothing,
-    depth = L3 > 0 ? attr.depth : nothing,
-    linesize = L3 > 0 ? attr.linesize : nothing,
-    associativity = L3 > 0 ? attr.associativity : nothing,
-    type = L3 > 0 ? attr.type_ : nothing
+    size = L3 ? attr.size : nothing,
+    depth = L3 ? attr.depth : nothing,
+    linesize = L3 ? attr.linesize : nothing,
+    associativity = L3 ? attr.associativity : nothing,
+    type = L3 ? attr.type_ : nothing
 )
 """
 L₁, L₂, L₃ cache size
